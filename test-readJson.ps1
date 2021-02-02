b673285a728e0a5650d00fb4e6044fb916aae51a
arielro85 <arielro85@gmail.com>
2021-02-02 15:04:45 -0300

    adds counting operations  with json tasks

--- test-readJson.ps1
+++ test-readJson.ps1
@@ -1,50 +1,70 @@
 $jsonFile = Get-Content .\settings.json | ConvertFrom-Json
 $sealOptions = $jsonFile.sealOptions 
+$operationsQtty= 0
 foreach ($setting in $sealOptions) {
     switch ($setting.psobject.Properties.Name) {
         regKeysToDelete {
+            $operationsQtty += ($setting.regKeysToDelete.Count)
             foreach ($key in $setting.regKeysToDelete) {
-                Write-Host $key.key
             
-            } 
+                switch ($key.value) {
+                    "novalue" {
+                        Write-Host "deleting item $($key.key) "
+                    }
+                    default {
+                        Write-Host "deleting item property $($key.value)"
+                    }
+                }
+                
+            }
         }
-        regKeysToAdd { Write-host "add" }
+        regKeysToAdd { 
+            $operationsQtty += ($setting.regKeysToAdd.Count)
+
+            Write-host "Adding Reg KEys" }
         servicesToCheck { 
+            $operationsQtty += ($setting.servicesToCheck.Count)
+
             foreach ($service in $setting.servicesToCheck) {
                 switch ($service.status) {
                     'disabled' {
-                        Write-host " disabling"
+                        Write-host "SERVICE disabling"
                         #Set-Service ($service.name) -StartupType Disabled
                         
                     }
                     'stop' {
-                        Write-host " st"
+                        Write-host "SERVICE Stopping"
 
                         #Stop-Service -Name ($service.name) -Force
                     }
                     'start' {
-                        Write-host " start"
+                        Write-host "SERVICE start"
 
                         #Start-Service -Name ($service.name)
                     }
                     'automatic' {
-                        Write-host " at"
+                        Write-host "SERVICE at"
 
                         #Set-Service ($service.name) -StartupType Automatic
                     }
                 }
             }
         }
-        filesToDelete {
-            
+        filesToDelete { 
+            $operationsQtty += ($setting.filesToDelete.Count)
+
+            foreach ($file in $setting.filesToDelete) {
+                Write-Host "removing file $($file.path)"
+            }
+            # Remove-Item -Path C:\Test\hidden-RO-file.txt -Force
         }
         Default {}
     }
 }
+write-host $operationsQtty
 
 
-
-
+ 
 
 function Seal-image {
     Param(
